71ac272297fdd0472cbc068f1e9ed2f04e0cffae
naga-03 <nagalakshmik3106@gmail.com>
2026-02-04 14:42:59 +0530

    persistence included

--- api.py
+++ api.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-app = FastAPI()
+import json
+from pathlib import Path
 
-students = []
-student_id = 1
+DATA_FILE = Path(__file__).parent/"students.json"
+def read_file():
+    with open(DATA_FILE,"r") as f:
+        return json.load(f)
+    
+def write_file(data):
+    with open(DATA_FILE,"w") as f:
+        json.dump(data,f,indent=4)
+    
+app = FastAPI()
 
 class StudentIn(BaseModel):
     name: str
@@ -15,45 +24,54 @@ class StudentOut(StudentIn):
     
 @app.post("/students",response_model=StudentOut)
 def create_student(student:StudentIn):
-    global student_id
+    data = read_file()
+    
+    new_id=data["last_id"]+1
     
     new_student = {
-        "id" : student_id,
+        "id" : new_id,
         "name":student.name,
         "department" : student.department,
         "year" : student.year      
     }
     
-    students.append(new_student)
-    student_id += 1
+    data["students"].append(new_student)
+    data["last_id"]=new_id
+    write_file(data)
     return new_student
 
 @app.get("/students",response_model=list[StudentOut])
 def list_all_student():
-    return students
+    data=read_file()
+    return data["students"]
 
 @app.get("/students/{student_id}",response_model=StudentOut)
 def get_student(student_id:int):
-    for student in students:
+    data=read_file()
+    for student in data["students"]:
         if student["id"]==student_id:
             return student
     raise HTTPException(status_code=404,detail="student not found")
         
 @app.put("/students/{student_id}",response_model=StudentOut)
 def update_student(student_id:int,updated_student:StudentIn):
-    for student in students:
+    data=read_file()
+    for student in data["students"]:
         if student["id"]==student_id:
             student["name"]=updated_student.name
             student["department"]=updated_student.department
             student["year"]=updated_student.year
+            write_file(data)
             return student
     raise HTTPException(status_code=404,detail="student not found")
 
 @app.delete("/students/{student_id}",response_model=StudentOut)
 def delete_student(student_id:int):
-    for index,student in enumerate (students):
+    data=read_file()
+    for index,student in enumerate (data["students"]):
         if student["id"]==student_id:
-            deleted_student = students.pop(index)
+            deleted_student=data["students"].pop(index)
+            write_file(data)
             return deleted_student
     raise HTTPException(status_code=404,detail="student not found")
         
